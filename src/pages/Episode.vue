b0e5fd64b93a403ec48018025cbcce62067c351f
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2020-10-11 23:38:57 -0300

    Alpha next episode

--- src/pages/Episode.vue
+++ src/pages/Episode.vue
@@ -1,6 +1,8 @@
 <template>
   <q-page>
-    <div id="dplayer"></div>
+    <div id="dplayer">
+      <q-btn icon="home" />
+    </div>
   </q-page>
 </template>
 
@@ -30,34 +32,57 @@ export default {
     }
   },
   mounted() {
-    this.player = new DPlayer({
-      container: document.getElementById("dplayer"),
-      video: {
-        autoplay: true,
-        preload: "auto",
-        url: `https://crunchyroll-quasar.herokuapp.com/${this.$store.state.api.episode.streams.streams.adaptive_dash["pt-BR"].url}`,
-        pic: this.$store.state.api.episode.images.thumbnail[0][4].source,
-        type: "customDash",
-        customType: {
-          customDash: function(video, player) {
-            dashjs
-              .MediaPlayer()
-              .create()
-              .initialize(video, video.src, false);
+    this.updatePlayer();
+  },
+  methods: {
+    updatePlayer() {
+      this.player = new DPlayer({
+        container: document.getElementById("dplayer"),
+        video: {
+          autoplay: true,
+          preload: "auto",
+          url: `https://crunchyroll-quasar.herokuapp.com/${this.$store.state.api.episode.streams.streams.adaptive_dash["pt-BR"].url}`,
+          pic: this.$store.state.api.episode.images.thumbnail[0][4].source,
+          type: "customDash",
+          customType: {
+            customDash: function(video, player) {
+              dashjs
+                .MediaPlayer()
+                .create()
+                .initialize(video, video.src, false);
+            }
           }
-        }
-      },
-      highlight: this.$store.state.api.episode.ad_breaks.map(ad => {
-        return {
-          text: "Skipper",
-          time: ad.offset_ms / 1000
-        };
-      })
-    });
-
-    this.player.on("canplay", () => {
-      this.player.play();
-    });
+        },
+        highlight: this.$store.state.api.episode.ad_breaks.map(ad => {
+          return {
+            text: "Skipper",
+            time: ad.offset_ms / 1000
+          };
+        }),
+        contextmenu: [
+          {
+            text: "Próximo episódio",
+            click: async player => {
+              let params = {
+                ...this.$route.params,
+                episode_id: this.$store.state.api.episode.next_episode_id
+              };
+              console.log(params);
+              this.$router.push({ params });
+              await this.$store.dispatch(
+                "api/SET_EPISODE",
+                this.$route.params.episode_id
+              );
+              this.updatePlayer();
+            }
+          }
+        ]
+      });
+      this.player.fullScreen.request("web");
+      this.player.on("canplay", () => {
+        // this.player.play();
+      });
+    }
   }
 };
 </script>

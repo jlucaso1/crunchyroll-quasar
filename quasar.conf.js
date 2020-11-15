213b73640ffc0e89f148fb965cc8c881edb51aaf
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2020-11-15 12:32:31 -0300

    Test

--- quasar.conf.js
+++ quasar.conf.js
@@ -156,7 +156,7 @@ module.exports = function(/* ctx */) {
         name: `Crunchyroll Quasar`,
         short_name: `Crunchyroll Quasar`,
         description: `Cópia da Crunchyroll feita em Vue`,
-        display: "fullscreen",
+        display: "minimal-ui",
         orientation: "portrait-primary",
         background_color: "#FC791E",
         theme_color: "#FC791E",

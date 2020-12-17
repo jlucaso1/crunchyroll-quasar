4a82c93bed638892c636a67c7c28a19d03826cb9
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2020-12-17 17:05:05 -0300

    Upgraded packages and added update

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -1,5 +1,6 @@
-import { register } from 'register-service-worker'
-
+import { register } from "register-service-worker";
+import { Notify } from "quasar";
+import { mdiCached } from "@quasar/extras/mdi-v5";
 // The ready(), registered(), cached(), updatefound() and updated()
 // events passes a ServiceWorkerRegistration instance in their arguments.
 // ServiceWorkerRegistration: https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration
@@ -11,31 +12,52 @@ register(process.env.SERVICE_WORKER_FILE, {
 
   // registrationOptions: { scope: './' },
 
-  ready (/* registration */) {
+  ready(/* registration */) {
     // console.log('Service worker is active.')
   },
 
-  registered (/* registration */) {
+  registered(/* registration */) {
     // console.log('Service worker has been registered.')
   },
 
-  cached (/* registration */) {
+  cached(/* registration */) {
     // console.log('Content has been cached for offline use.')
   },
 
-  updatefound (/* registration */) {
+  updatefound(/* registration */) {
     // console.log('New content is downloading.')
   },
 
-  updated (/* registration */) {
-    // console.log('New content is available; please refresh.')
+  updated(/* registration */) {
+    Notify.create({
+      color: "negative",
+      icon: mdiCached,
+      message: "Updated content is available. Please refresh the page.",
+      timeout: 0,
+      multiLine: true,
+      position: "top",
+      actions: [
+        {
+          label: "Refresh",
+          color: "yellow",
+          handler: () => {
+            window.location.reload();
+          }
+        },
+        {
+          label: "Dismiss",
+          color: "white",
+          handler: () => {}
+        }
+      ]
+    });
   },
 
-  offline () {
+  offline() {
     // console.log('No internet connection found. App is running in offline mode.')
   },
 
-  error (/* err */) {
+  error(/* err */) {
     // console.error('Error during service worker registration:', err)
   }
-})
+});
